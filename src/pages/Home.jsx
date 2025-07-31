4733a41c9e2d8ffc2256d69ec048d08957adf57b
Malcolm <kagsmalcolm@gmail.com>
2025-07-31 19:34:41 +0300

    finish basic routes lab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((r) => r.json())
+      .then(setMovies);
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        <div>
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} id={movie.id} title={movie.title} />
+          ))}
+        </div>
       </main>
     </>
   );
-};
+}
 
 export default Home;
